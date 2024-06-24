d0f0ffa6f1dc6a3bf577e30004c84bd2d07b2ab5
Atmos <kzlouge@proton.me>
2024-06-24 17:27:03 +0800

    remove nvim

--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.del("n", "<leader>l")
-vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<cr>", { desc = "Lazy" })
